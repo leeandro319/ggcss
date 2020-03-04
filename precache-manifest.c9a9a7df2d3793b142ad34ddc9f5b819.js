b18c4c0c1dc4a9b192cb4f360b9abe5f859d6fc2
Leandro Guaraldi <leandro_319@yahoo.com.br>
2020-03-04 00:28:55 -0300

    Updates

---
+++ precache-manifest.c9a9a7df2d3793b142ad34ddc9f5b819.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05f14a30b4661b395296b25e3e943e7a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0b2bf32cbed0c6a3925d",
+    "url": "/static/css/main.7900971c.chunk.css"
+  },
+  {
+    "revision": "358b4578e3326f094886",
+    "url": "/static/js/2.75cea26a.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.75cea26a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b2bf32cbed0c6a3925d",
+    "url": "/static/js/main.d00cbcc7.chunk.js"
+  },
+  {
+    "revision": "27f531c7a141d4e458e7",
+    "url": "/static/js/runtime-main.25ea7bba.js"
+  }
+]);
